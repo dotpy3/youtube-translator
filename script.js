86c61ed0fa904e09922b67c0f3e369cfeb7f212f
Eric Gourlaouen <eric@thethingsnetwork.org>
2017-09-16 11:18:50 +0200

    The extension now makes sounds 👏

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var button = document.getElementById('button')
+if (!button) {
+  alert("WOW NO RESULT FOUND")
+}
+
+button.addEventListener('click', () => {
+  play()
+})
+
 function play() {
   responsiveVoice.speak('Hello World')
 }
