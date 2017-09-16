92ff4c72bd187de390319731ae0cbdb4fa34a44c
Eric Gourlaouen <eric@thethingsnetwork.org>
2017-09-16 15:19:04 +0200

    Add voices

---
+++ voices.js
@@ -0,0 +1,12 @@
+var languageToVoice = {
+  de: "Deutsch Female",
+  en: "UK English Female",
+  es: "Spanish Female",
+  fr: "French Female",
+  it: "Italian Female",
+  jp: "Japanese Female",
+  kr: "Korean Female",
+  nl: "Dutch Female",
+  ru: "Russian Female",
+  se: "Swedish Female",
+}
