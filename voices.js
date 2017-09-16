6c3cd45a1693a85b6adbce4e6a85077c0e27dd53
Eric Gourlaouen <eric@thethingsnetwork.org>
2017-09-16 15:47:22 +0200

    cleaning voices.js 🚽

--- voices.js
+++ voices.js
@@ -1,21 +1,8 @@
-var languageToVoice = {
-  de: "Deutsch Female",
-  en: "UK English Female",
-  es: "Spanish Female",
-  fr: "French Female",
-  it: "Italian Female",
-  jp: "Japanese Female",
-  kr: "Korean Female",
-  nl: "Dutch Female",
-  ru: "Russian Female",
-  se: "Swedish Female",
-}
-
-function userLanguage() {
+function userLanguageInFiveCharacters() {
   return window.navigator.language
 }
 
-function userLanguageForTranslator() {
+function userLanguageInTwoCharacters() {
   var lang = userLanguage()
   var twoCharacters = lang.match(/^.{2}/g)[0]
   return twoCharacters
@@ -24,7 +11,7 @@ function userLanguageForTranslator() {
 var synth = window.speechSynthesis
 var voices = synth.getVoices()
 
-function Speak(language, text) {
+function Speak(languageInFiveCharacters, text) {
   for (var voice of voices) {
     if (voice.lang === language) {
       var utterance = new SpeechSynthesisUtterance(text)
