fd21a6bfcd252c5e6b2527e376f46040e97e97a3
Eric Gourlaouen <eric@thethingsnetwork.org>
2017-09-16 16:39:37 +0200

    Do on the whole song

--- xml_extractor.js
+++ xml_extractor.js
@@ -2,7 +2,6 @@ function fetch_transcript(language, video_identifier) {
     var xml = xml_extractor_http_get(language, video_identifier)
     var transcript = xml.getElementsByTagName('transcript')[0]
     for (var i = 0; i < transcript.childNodes.length; i++) {
-    // for (var i = 0; i < ; i++) {
         var childNode = transcript.childNodes[i]
         var start_time = childNode.getAttribute('start')
         var duration = childNode.getAttribute('dur')
