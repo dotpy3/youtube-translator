c351e55d72abd40001a75a21850477cce70f1a80
physicalattraction <erwin.rossen@gmail.com>
2017-09-16 11:24:22 +0200

    Play multiple sentences

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ button.addEventListener('click', () => {
   play()
 })
 
-function play() {
-  responsiveVoice.speak('Hello World')
+function play(video_identifier) {
+    fetch_transcript('en', video_identifier);
+    // play_sentence(video_identifier)
 }
+
+function fetch_transcript(language, video_identifier) {
+    url = "http://video.google.com/timedtext?lang=" + language + "&v=" + video_identifier
+    var x = new XMLHttpRequest();
+    x.open("GET", url, true);
+    x.onreadystatechange = function () {
+      if (x.readyState == 4 && x.status == 200)
+      {
+        var doc = x.responseXML;
+        document.write(doc)
+      }
+    };
+}
+
+function play_sentence(text) {
+    document.write(text + "\n")
+    responsiveVoice.speak(text)
